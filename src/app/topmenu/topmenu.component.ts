a57814e2d581f4eb283530a26b62d78740a8cf7f
Pieter van Mill <pietervanmill@hotmail.com>
2017-08-10 11:33:16 +0200

    Mobiele weergave toegevoegd en menu verbeterd voor mobiel.

--- src/app/topmenu/topmenu.component.ts
+++ src/app/topmenu/topmenu.component.ts
@@ -7,9 +7,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TopmenuComponent implements OnInit {
 
+  opened = false;
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  openNavigation() {
+    this.opened = true;
+  }
+
+  closeNavigation() {
+    this.opened = false;
   }
 
 }
