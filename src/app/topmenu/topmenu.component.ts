9418e5bb88b26f352c1b2e6c848345d86b3b5240
Pieter van Mill <pietervanmill@hotmail.com>
2017-08-10 12:49:46 +0200

    Menu gaat weg bij navigeren en je kan nu naast het menu klikken om het te sluiten.

--- src/app/topmenu/topmenu.component.ts
+++ src/app/topmenu/topmenu.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit} from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-topmenu',
@@ -7,11 +8,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TopmenuComponent implements OnInit {
 
+  elementRef: any;
   opened = false;
 
-  constructor() { }
+  constructor(private router: Router) {
+  }
 
   ngOnInit() {
+
+    // Als er een router change is sluit de navigatie
+    this.router.events.subscribe((val) => {
+      // see also
+      this.closeNavigation();
+      console.log('Go to ohter route');
+    });
   }
 
   openNavigation() {
