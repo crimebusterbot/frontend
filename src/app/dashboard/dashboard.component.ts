ccff762a57930f98dcfb6643cd235d4c4e385775
pcmill <gitlab@pietervanmill.nl>
2019-01-12 19:03:26 +0100

    Added the planned counts on the dashboard page.

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -8,19 +8,21 @@ import {DataService} from '../_services/data.service';
 })
 export class DashboardComponent implements OnInit {
   public total: number;
+  public good: number;
+  public fake: number;
 
   constructor(private dataService: DataService) { }
 
   ngOnInit() {
     this.dataService.getTotal().subscribe(
       data => {
-        this.total = data.count;
+        this.total = data.total;
+        this.good = data.good;
+        this.fake = data.fake;
       },
       error => {
         console.log(error);
       }
     );
   }
-
-
 }
